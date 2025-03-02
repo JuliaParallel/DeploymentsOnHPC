603657df166dbfd993f512aae4c3ec9369e8d260
Johannes Blaschke <jpblaschke@lbl.gov>
2025-03-02 08:31:37 -0800

    add cleanup step

--- nersc/util/get_mpi_settings.jl
+++ nersc/util/get_mpi_settings.jl
@@ -5,6 +5,15 @@ Pkg.activate(joinpath(@__DIR__, "mpi_proj"))
 Pkg.instantiate()
 
 
+# Ensure that any pre-existing LocalPreferences.toml files are removed from the
+# environment first:
+local_prefs_file = joinpath(@__DIR__, "mpi_proj", "LocalPreferences.toml")
+if isfile(local_prefs_file)
+    @info("$(local_prefs_file) exists from previous run => deleting ")
+    rm(local_prefs_file)
+end
+
+
 using MPIPreferences
 
 

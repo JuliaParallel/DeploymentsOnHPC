5526d75677db11e4f0488ee202eae6409573954a
Johannes Blaschke <jpblaschke@lbl.gov>
2025-03-03 10:57:57 -0800

    dont use relative paths

--- nersc/julia/sm-config/install.sh
+++ nersc/julia/sm-config/install.sh
@@ -1,5 +1,11 @@
 set -eu
 
+# Erase previous juliup database (this will run a fresh install every time)
+if [[ -e ${JULIAUP_DEPOT_PATH}/juliaup/juliaup.json ]]
+then
+    rm ${JULIAUP_DEPOT_PATH}/juliaup/juliaup.json
+fi
+
 # Depends on the juliaup module already being loaded:
 module load juliaup/stable
 

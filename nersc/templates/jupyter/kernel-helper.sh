28811ca5c4c18698b0b7a676b8a1e3e0dc548d23
Johannes Blaschke <jpblaschke@lbl.gov>
2025-03-03 14:48:17 -0800

    add kernels

---
+++ nersc/templates/jupyter/kernel-helper.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+{{#use_latest}}
+module use /global/common/software/nersc9/julia/modules
+{{/use_latest}}
+module load PrgEnv-gnu
+module load python
+module load julia/{{{julia_version}}}
+{{#use_cudnn}}
+module load cudnn
+{{/use_cudnn}}
+
+export JULIA_NUM_THREADS={{julia_thread_ct}}
+
+readarray -t ijulia_boostrap < <(julia {{{nersc_resource_dir}}}/bootstrap.jl)
+
+echo "Check-and-install returned following output:"
+_IFS=$IFS
+IFS=$'\n'
+for each in ${ijulia_boostrap[*]}
+do
+    echo $each
+done
+IFS=$_IFS
+
+JULIA_EXEC=$(which julia)
+KERNEL="${ijulia_boostrap[-1]}"
+
+echo "Connecting using JULIA_EXEC=$JULIA_EXEC and KERNEL=$KERNEL"
+exec $JULIA_EXEC -i --startup-file=yes --color=yes $KERNEL "$@"
--- nersc/templates/render.sh
+++ nersc/templates/render.sh
@@ -2,129 +2,90 @@
 set -eu
 
 
-# An implementation of realpath for systems which don't have it, or a different
-# implementation of it. This will attempt traverse the path and read any
-# relative redirection using modern bash language only. This will not resolve
-# symlinks as they could be relative or absolute symlinks. Usage:
-#
-#    $ __real_path /path/to/../folder/./with/relative/bits
-#    $ /path/to/folder/with/relative/bits
-#
-__real_path () {
-    # Redefined IFS to treat "/" as a separator.
-    IFS="/" read -ra input_path <<< "$1";
+pushd ${__DIR__}
+    #---------------------------------------------------------------------------
+    # Julia Environments
+    #---------------------------------------------------------------------------
+    ${__PREFIX__}/opt/bin/simple-templates.ex                                  \
+        environment/LocalPreferences.toml                                      \
+        aocc_settings.toml                                                     \
+        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/LocalPreferences.toml"
+    ${__PREFIX__}/opt/bin/simple-templates.ex                                  \
+        environment/Project.toml                                               \
+        aocc_settings.toml                                                     \
+        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/Project.toml"
 
-    # Define 'real'
-    local real=""
-    # Iterate over path elements using "/" (set above) as a separator
-    for elt in ${input_path[@]}
-    do
-        # Resolve relative path punctuation.
-        if [[ "$elt" == "." || -z "$elt" ]]
-        then
-            # "." is a noop
-            continue
-        elif [[ "$elt" == ".." ]]
-        then 
-            # ".." => remove the last element
-            real="${real%%/${real##*/}}"
-            continue
-        else
-            # advance to the next directory
-            real="${real}/${elt}"
-        fi
+    ${__PREFIX__}/opt/bin/simple-templates.ex                                  \
+        environment/LocalPreferences.toml                                      \
+        cray_settings.toml                                                     \
+        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/LocalPreferences.toml"
+    ${__PREFIX__}/opt/bin/simple-templates.ex                                  \
+        environment/Project.toml                                               \
+        cray_settings.toml                                                     \
+        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/Project.toml"
+
+    ${__PREFIX__}/opt/bin/simple-templates.ex                                  \
+        environment/LocalPreferences.toml                                      \
+        gnu_settings.toml                                                      \
+        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/LocalPreferences.toml"
+    ${__PREFIX__}/opt/bin/simple-templates.ex                                  \
+        environment/Project.toml                                               \
+        gnu_settings.toml                                                      \
+        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/Project.toml"
+
+    ${__PREFIX__}/opt/bin/simple-templates.ex                                  \
+        environment/LocalPreferences.toml                                      \
+        intel_settings.toml                                                    \
+        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/LocalPreferences.toml"
+    ${__PREFIX__}/opt/bin/simple-templates.ex                                  \
+        environment/Project.toml                                               \
+        intel_settings.toml                                                    \
+        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/Project.toml"
+
+    ${__PREFIX__}/opt/bin/simple-templates.ex                                  \
+        environment/LocalPreferences.toml                                      \
+        nvidia_settings.toml                                                   \
+        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/LocalPreferences.toml"
+    ${__PREFIX__}/opt/bin/simple-templates.ex                                  \
+        environment/Project.toml                                               \
+        nvidia_settings.toml                                                   \
+        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/Project.toml"
+
+    ${__PREFIX__}/opt/bin/simple-templates.ex                                  \
+        environment/LocalPreferences.toml                                      \
+        llvm_settings.toml                                                     \
+        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/LocalPreferences.toml"
+    ${__PREFIX__}/opt/bin/simple-templates.ex                                  \
+        environment/Project.toml                                               \
+        llvm_settings.toml                                                     \
+        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/Project.toml"
+
+    ${__PREFIX__}/opt/bin/simple-templates.ex                                  \
+        environment/LocalPreferences.toml                                      \
+        openmpi_settings.toml                                                  \
+        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/LocalPreferences.toml"
+    ${__PREFIX__}/opt/bin/simple-templates.ex                                  \
+        environment/Project.toml                                               \
+        openmpi_settings.toml                                                  \
+        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/Project.toml"
+
+    #---------------------------------------------------------------------------
+    # Jupyter NERSC-JULIA Kernels
+    #---------------------------------------------------------------------------
+
+    # WARNING: this requires the jupyter_settings.toml/nersc_resource_dir to be
+    # set correctly. TODO: automate in the future.
+    ${__PREFIX__}/opt/bin/simple-templates.ex                                  \
+        jupyter/kernel.json                                                    \
+        jupyter_settings.toml                                                  \
+        "../kernels/rendered/nersc-julia-{{julia_thread_ct}}-{{julia_version}}{{#use_cudnn}}-cudnn{{/use_cudnn}}{{#use_latest}}-beta{{/use_latest}}/kernel.json"
+    ${__PREFIX__}/opt/bin/simple-templates.ex                                  \
+        jupyter/kernel-helper.sh                                               \
+        jupyter_settings.toml                                                  \
+        "../kernels/rendered/nersc-julia-{{julia_thread_ct}}-{{julia_version}}{{#use_cudnn}}-cudnn{{/use_cudnn}}{{#use_latest}}-beta{{/use_latest}}/kernel-helper.sh"
+    for target in $(/bin/ls ../kernels/rendered/)
+    do 
+        cp jupyter/logo-32x32.png ../kernels/rendered/$target/
+        cp jupyter/logo-64x64.png ../kernels/rendered/$target/
     done
-
-    echo "$real"
-}
-
-
-# Returns the parent directory path of the first argument. Usage:
-#
-#    $ __dir_name /path/to/script.sh
-#    $ /path/to
-#
-__dir_name () {
-    echo "${1%/*}"
-}
-
-
-# Holds the script prefix -- this is an array with the last element
-# corresponding to the current script
-if ! [[ -v __PREFIX ]]
-then
-    export __PREFIX=()
-fi
-# Add current script's directory to prefix
-__PREFIX+=($(                                  \
-    __dir_name $(                              \
-        __real_path "${PWD}/${BASH_SOURCE[0]}" \
-    )                                          \
-))
-
-
-pushd ${__PREFIX[-1]}
-    ../../opt/bin/simple-templates.ex                                          \
-        environment/LocalPreferences.toml                                      \
-        aocc_settings.toml                                                     \
-        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/LocalPreferences.toml"
-    ../../opt/bin/simple-templates.ex                                          \
-        environment/Project.toml                                               \
-        aocc_settings.toml                                                     \
-        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/Project.toml"
-
-    ../../opt/bin/simple-templates.ex                                          \
-        environment/LocalPreferences.toml                                      \
-        cray_settings.toml                                                     \
-        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/LocalPreferences.toml"
-    ../../opt/bin/simple-templates.ex                                          \
-        environment/Project.toml                                               \
-        cray_settings.toml                                                     \
-        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/Project.toml"
-
-    ../../opt/bin/simple-templates.ex                                          \
-        environment/LocalPreferences.toml                                      \
-        gnu_settings.toml                                                      \
-        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/LocalPreferences.toml"
-    ../../opt/bin/simple-templates.ex                                          \
-        environment/Project.toml                                               \
-        gnu_settings.toml                                                      \
-        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/Project.toml"
-
-    ../../opt/bin/simple-templates.ex                                          \
-        environment/LocalPreferences.toml                                      \
-        intel_settings.toml                                                    \
-        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/LocalPreferences.toml"
-    ../../opt/bin/simple-templates.ex                                          \
-        environment/Project.toml                                               \
-        intel_settings.toml                                                    \
-        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/Project.toml"
-
-    ../../opt/bin/simple-templates.ex                                          \
-        environment/LocalPreferences.toml                                      \
-        nvidia_settings.toml                                                   \
-        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/LocalPreferences.toml"
-    ../../opt/bin/simple-templates.ex                                          \
-        environment/Project.toml                                               \
-        nvidia_settings.toml                                                   \
-        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/Project.toml"
-
-    ../../opt/bin/simple-templates.ex                                          \
-        environment/LocalPreferences.toml                                      \
-        llvm_settings.toml                                                     \
-        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/LocalPreferences.toml"
-    ../../opt/bin/simple-templates.ex                                          \
-        environment/Project.toml                                               \
-        llvm_settings.toml                                                     \
-        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/Project.toml"
-
-    ../../opt/bin/simple-templates.ex                                          \
-        environment/LocalPreferences.toml                                      \
-        openmpi_settings.toml                                                  \
-        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/LocalPreferences.toml"
-    ../../opt/bin/simple-templates.ex                                          \
-        environment/Project.toml                                               \
-        openmpi_settings.toml                                                  \
-        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/Project.toml"
 popd
-

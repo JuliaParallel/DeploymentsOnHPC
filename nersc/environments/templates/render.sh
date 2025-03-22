5851d2ded39e503d9743b9eb08b16dd8654d9759
Johannes Blaschke <jpblaschke@lbl.gov>
2025-03-22 09:21:29 -0700

    move template dir to more appropriate location

---
+++ nersc/environments/templates/render.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -eu
+pushd ${__DIR__}
+
+prg_envs=("aocc" "cray" "gnu" "nvidia" "llvm" "openmpi")
+
+#-------------------------------------------------------------------------------
+# Julia Environments
+#-------------------------------------------------------------------------------
+
+for prg_env in ${prg_envs[@]}
+do
+    ${__PREFIX__}/opt/bin/simple-templates.ex                                  \
+        environment/LocalPreferences.toml                                      \
+        "${prg_env}_settings.toml"                                             \
+        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/LocalPreferences.toml"
+    ${__PREFIX__}/opt/bin/simple-templates.ex                                  \
+        environment/Project.toml                                               \
+        "${prg_env}_settings.toml"                                             \
+        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/Project.toml"
+done
+
+popd

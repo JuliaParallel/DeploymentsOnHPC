b8beae3b194a863cb84cb9c44e307691b1b1e50d
Johannes Blaschke <jpblaschke@lbl.gov>
2025-03-30 13:48:37 -0700

    update production renderer

--- nersc/mpiwrapper/render.sh
+++ nersc/mpiwrapper/render.sh
@@ -2,6 +2,13 @@
 set -eu
 
 pushd ${__DIR__}
-    ${__PREFIX__}/opt/bin/simple-modules.ex sm-config
+
+prg_envs=("aocc" "cray" "gnu" "intel" "nvidia" "llvm" "openmpi")
+
+for prg_env in ${prg_envs[@]}
+do
+    ${__PREFIX__}/opt/bin/simple-modules.ex sm-config --variant=${prg_env}
+done
+
 popd
 

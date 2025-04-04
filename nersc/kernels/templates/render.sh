ede4006c467f50e83f150dafbc591e20ae2da3a2
Johannes Blaschke <jpblaschke@lbl.gov>
2025-04-03 19:58:32 -0700

    use new templates

--- nersc/kernels/templates/render.sh
+++ nersc/kernels/templates/render.sh
@@ -9,20 +9,15 @@ pushd ${__DIR__}
 KERNEL_DIR="/global/common/software/nersc9/julia/kernels"
 mkdir -p ${KERNEL_DIR}
 
-${__PREFIX__}/opt/bin/simple-templates.ex                                  \
-    jupyter/kernel.json                                                    \
-    settings.toml                                                          \
-    "${KERNEL_DIR}/rendered/nersc-julia-{{julia_thread_ct}}-{{julia_version}}{{#use_latest}}-beta{{/use_latest}}/kernel.json"
-${__PREFIX__}/opt/bin/simple-templates.ex                                  \
-    jupyter/kernel-helper.sh                                               \
-    settings.toml                                                          \
-    "${KERNEL_DIR}/rendered/nersc-julia-{{julia_thread_ct}}-{{julia_version}}{{#use_latest}}-beta{{/use_latest}}/kernel-helper.sh"
+${__PREFIX__}/opt/bin/simple-templates.ex                                          \
+    --dir --resource "^jupyter/.*.png$"                                            \
+    jupyter                                                                        \
+    settings.toml                                                                  \
+    "${KERNEL_DIR}/rendered/nersc-julia-{{julia_thread_ct}}-{{julia_version}}{{#use_latest}}-beta{{/use_latest}}"
 
 for target in $(/bin/ls ${KERNEL_DIR}/rendered/)
 do 
     chmod u+x ${KERNEL_DIR}/rendered/$target/kernel-helper.sh
-    cp jupyter/logo-32x32.png ${KERNEL_DIR}/rendered/${target}/
-    cp jupyter/logo-64x64.png ${KERNEL_DIR}/rendered/${target}/
 done
 
 cp    ${__DIR__}/../bootstrap.jl ${KERNEL_DIR}/
--- nersc/kernels/templates/render_tmp.sh
+++ nersc/kernels/templates/render_tmp.sh
@@ -11,20 +11,14 @@ mkdir -p ${KERNEL_DIR}
 
 ${__PREFIX__}/opt/bin/simple-templates.ex                                          \
     --overwrite "{\"nersc_resource_dir\": \"${KERNEL_DIR}\"}"                      \
-    jupyter/kernel.json                                                            \
+    --dir --resource "^jupyter/.*.png$"                                            \
+    jupyter                                                                        \
     settings.toml                                                                  \
-    "${KERNEL_DIR}/rendered/nersc-julia-{{julia_thread_ct}}-{{julia_version}}{{#use_latest}}-beta{{/use_latest}}/kernel.json"
-${__PREFIX__}/opt/bin/simple-templates.ex                                          \
-    --overwrite "{\"nersc_resource_dir\": \"${KERNEL_DIR}\"}"                      \
-    jupyter/kernel-helper.sh                                                       \
-    settings.toml                                                                  \
-    "${KERNEL_DIR}/rendered/nersc-julia-{{julia_thread_ct}}-{{julia_version}}{{#use_latest}}-beta{{/use_latest}}/kernel-helper.sh"
+    "${KERNEL_DIR}/rendered/nersc-julia-{{julia_thread_ct}}-{{julia_version}}{{#use_latest}}-beta{{/use_latest}}"
 
 for target in $(/bin/ls ${KERNEL_DIR}/rendered/)
 do 
     chmod u+x ${KERNEL_DIR}/rendered/$target/kernel-helper.sh
-    cp jupyter/logo-32x32.png ${KERNEL_DIR}/rendered/${target}/
-    cp jupyter/logo-64x64.png ${KERNEL_DIR}/rendered/${target}/
 done
 
 cp    ${__DIR__}/../bootstrap.jl ${KERNEL_DIR}/

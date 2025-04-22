a656af737406dfed7815c220f9d898b14d7cb58d
Johannes Blaschke <jpblaschke@lbl.gov>
2025-04-21 20:58:55 -0700

    explicitly depend on cudatoolkit

--- nersc/juliaup/sm-config/module_template.lua
+++ nersc/juliaup/sm-config/module_template.lua
@@ -8,6 +8,15 @@ whatis("Version: {{{INSTALL_VERSION}}}")
 whatis("URL: https://github.com/JuliaLang/juliaup")
 
 -------------------------------------------------------------------------------
+--- Requirements on External Dependencies
+-------------------------------------------------------------------------------
+
+-- ensure that cudatoolkit is loaded -- even if the user load the `cpu` module,
+-- as some Julia packages will depend on CUDA (and we can't control that these
+-- don't get loaded from the module)
+depends_on("cudatoolkit")
+
+-------------------------------------------------------------------------------
 --- JULIAUP Binary
 -------------------------------------------------------------------------------
 
@@ -16,26 +25,43 @@ append_path("PATH", "{{{BIN_PATH}}}")
 -------------------------------------------------------------------------------
 --- Site-Wide Julia Settings
 --
--- Julia has a mechanism by which settings can be injectect into the
+-- Julia has a mechanism by which settings can be injected into the
 -- Preferences.jl stack. This is done by creating a system-wide Julia project
--- contianing a LocalPreferences.toml file and ensurign that this project is on
+-- containing a LocalPreferences.toml file and ensuring that this project is on
 -- the user's JULIA_LOAD_PATH.
---
--- At NERSC there are several different versions of these site-wide settings in
--- order to account for different MPI, and CUDA versions.
 -------------------------------------------------------------------------------
 
--- PE_ENV, FAMILY_MPI, and FAMILY_CUDATOOLKIT are environment variables set by
--- Lmod. We use these to discriminate between different site-wide settings (see
--- below), which depend on the PrgEnv, MPI version, and CUDA toolkit versions
--- respectively.
+-- At NERSC there are several different versions of these site-wide settings in
+-- order to account for different MPI (detected using the `PE_ENV`, and
+-- `FAMILY_MPI` env vars), and CUDA versions (provided by the `cudatoolkit`
+-- module, which sets the `FAMILY_CUDATOOLKIT` env var).
 local PE_ENV = os.getenv("PE_ENV"):lower()
 local FAMILY_MPI = os.getenv("LMOD_FAMILY_MPI"):lower()
-local FAMILY_CUDATOOLKIT = os.getenv("LMOD_FAMILY_CUDATOOLKIT_VERSION"):lower()
+local FAMILY_CUDATOOLKIT = os.getenv("LMOD_FAMILY_CUDATOOLKIT_VERSION")
+if nil == FAMILY_CUDATOOLKIT then
+    -- workaround for when `cudatoolkit` gets unloaded by another module
+    -- _after_ this module has been loaded. If we ever implement a site hook
+    -- which will re-load the `juliaup` module after cudatoolkit is unloaded
+    -- then this warning can be removed.
+    LmodWarning([[
+Some Julia packages require that the `cudatoolkit` module is loaded on
+Perlmutter. If a dependency requires CUDA, then you will see the following
+error: `Error: CUDA.jl could not find an appropriate CUDA runtime to use.`
+You can fix this error by running:
+```
+module load cudatoolkit/{{{DEFAULT_CUDATOOLKIT_VERSION}}}
+```
+    ]])
+    -- AFAIK there is no way to automatically trigger Lmod to now load the
+    -- `cudatoolkit` module. So we'll set `FAMILY_CUDATOOLKIT` to a default
+    -- value and print a warning.
+    FAMILY_CUDATOOLKIT = "{{{DEFAULT_CUDATOOLKIT_VERSION}}}"
+end
+FAMILY_CUDATOOLKIT = FAMILY_CUDATOOLKIT:lower()
 -- Julia environment name: {PE}.{MPI}.{CUDA}
 local ENV_NAME = PE_ENV .. "." .. FAMILY_MPI .. ".cuda" .. FAMILY_CUDATOOLKIT
--- The JULIA_LOAD_PATH depends on the PE and cudatoolkit modules loaded. If the
--- user loads juliaup, and then changes one of these, the value that
+-- The `JULIA_LOAD_PATH` depends on the PE and cudatoolkit modules loaded. If
+-- the user loads `juliaup`, and then changes one of these, the value that
 -- `JULIA_LOAD_PATH` is set to will change. This potentially causes Lmod to
 -- loose track of what changes it has made to the env => we "remember" what
 -- `JULIA_LOAD_PATH` was set to by using the `__JULIAUP_MODULE_JULIA_LOAD_PATH`
@@ -72,7 +98,7 @@ append_path("JULIA_LOAD_PATH", JULIA_LOAD_PATH)
 -------------------------------------------------------------------------------
 --- NERSC-Specific Helper and Utility Scripts
 --
--- NERSC has a couple utlity scripts (often in response of tickets), eg.
+-- NERSC has a couple utility scripts (often in response of tickets), e.g.
 -- generate custom Jupyter kernels.
 -------------------------------------------------------------------------------
 

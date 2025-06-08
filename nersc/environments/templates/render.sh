d24e99265379e78f4751c2ce1577fa2e7e3814a3
Johannes Blaschke <jpblaschke@lbl.gov>
2025-06-08 08:58:39 -0700

    update file permissions

--- nersc/environments/templates/render.sh
+++ nersc/environments/templates/render.sh
@@ -11,6 +11,7 @@ prg_envs=("aocc" "cray" "gnu" "intel" "nvidia" "llvm")
 for prg_env in ${prg_envs[@]}
 do
     ${__PREFIX__}/opt/bin/simple-templates.ex --dir                 \
+        --chmod "g+rX,o+rX,g-w,o-w" --chmod-basename                \
         environment                                                 \
         "${prg_env}_settings.toml"                                  \
         "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}"

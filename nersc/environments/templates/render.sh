a31452416aaeb06ef6d1b8317a5ce2e36d71d6a4
Johannes Blaschke <jpblaschke@lbl.gov>
2025-06-07 18:08:43 -0700

    use template dir mode

--- nersc/environments/templates/render.sh
+++ nersc/environments/templates/render.sh
@@ -10,14 +10,10 @@ prg_envs=("aocc" "cray" "gnu" "intel" "nvidia" "llvm" "openmpi")
 
 for prg_env in ${prg_envs[@]}
 do
-    ${__PREFIX__}/opt/bin/simple-templates.ex                                  \
-        environment/LocalPreferences.toml                                      \
-        "${prg_env}_settings.toml"                                             \
-        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/LocalPreferences.toml"
-    ${__PREFIX__}/opt/bin/simple-templates.ex                                  \
-        environment/Project.toml                                               \
-        "${prg_env}_settings.toml"                                             \
-        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}/Project.toml"
+    ${__PREFIX__}/opt/bin/simple-templates.ex --dir                 \
+        environment                                                 \
+        "${prg_env}_settings.toml"                                  \
+        "../rendered/{{prg_env}}.{{mpi_mod}}.cuda{{cuda_version}}"
 done
 
 popd
